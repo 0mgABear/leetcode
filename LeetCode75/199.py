4675aeb3ec0e9c4161445d2b4795adf5c35615aa
0mgABear <yeoyi94@gmail.com>
2026-05-26 20:00:00 +0800

    LC75

---
+++ LeetCode75/199.py
@@ -0,0 +1,66 @@
+# 199. Binary Tree Right Side View
+
+# Given the root of a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+
+ 
+
+# Example 1:
+
+# Input: root = [1,2,3,null,5,null,4]
+
+# Output: [1,3,4]
+
+# Explanation:
+
+
+
+# Example 2:
+
+# Input: root = [1,2,3,4,null,null,null,5]
+
+# Output: [1,3,4,5]
+
+# Explanation:
+
+
+
+# Example 3:
+
+# Input: root = [1,null,3]
+
+# Output: [1,3]
+
+# Example 4:
+
+# Input: root = []
+
+# Output: []
+
+ 
+
+# Constraints:
+
+# The number of nodes in the tree is in the range [0, 100].
+# -100 <= Node.val <= 100
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        queue = deque([root])
+        output = []
+        while queue:
+            level_size = len(queue)
+            
+            for i in range(level_size):
+                node = queue.popleft()
+                
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+                
+                if i == level_size - 1:
+                    output.append(node.val)
+
+        return output
